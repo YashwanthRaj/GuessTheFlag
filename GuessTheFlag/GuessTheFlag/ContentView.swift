ec00ce6a882be56579994b3b93db0542bfe8cba2
YashwanthRaj <yashwanthraj6383@gmail.com>
2024-06-28 11:21:37 -0400

    Part Two
    This version has a working version of the app with styling and function to go to next question and continuing the quiz.

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -3,116 +3,88 @@
 //  GuessTheFlag
 //
 //  Created by Yashwanth Raj Varadharajan on 6/27/24.
-//  PROJECT TWO PART ONE
+//  PROJECT TWO PART TWO
 
 import SwiftUI
 
 struct ContentView: View {
-    @State private var alertButton = false
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var corretAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
     
     var body: some View {
         
-        //        VStack(alignment: .leading) {
-        //            Text("Hello World!!")
-        //            // Spacer()
-        //            Text("My name is Yashwanth Raj Varadharajan")
-        //        }
-        
-        //        ZStack{
-        //            // Color.primary // These will change according to system
-        //            Color.red  // This is a view of its own
-        //                //.frame(width: 200, height: 200)
-        //                .frame(minWidth: 200, maxWidth: .infinity, maxHeight: 300)
-        //            Text("Hello again!")
-        //        }// .ignoresSafeArea() // This will ignore safe area and expand to the full
-        
-        
-//        ZStack{
-//            VStack(spacing: 0){
-//                Color.red
-//                Color.blue
-//            }
-//            
-//            Text("Hello World")
-//                .foregroundStyle(.secondary)
-//                .padding(50)
-//                .background(.ultraThinMaterial)
-//            
-//        }.ignoresSafeArea()
-        
-        // LinearGradient(colors: [.white, .black], startPoint: .top, endPoint: .bottom)
-        
-        // RadialGradient(colors: [.blue,.black], center: .center, startRadius: 20, endRadius: 200)
-        
-        // AngularGradient(colors: [.red, .yellow, .green, .blue, .orange, .purple], center: .center)
-        
-//        Text("Helloo")
-//            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
-//            .foregroundStyle(.white)
-//            .background(.indigo.gradient)
-        
-        //Button("Delete selection",role: .destructive, action: deleteAction)
-        
-        VStack{
-            Button("Button 1") { }
-                .buttonStyle(.bordered)
+        ZStack{
+            // LinearGradient(colors: [.blue,.white], startPoint: .top, endPoint: .bottom).ignoresSafeArea()
+            RadialGradient(stops: [
+                .init(color: .blue, location: 0.3),
+                .init(color: .red, location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 700)
+                .ignoresSafeArea()
             
-            Button("Button 2", role: .destructive) { }
-                .buttonStyle(.bordered)
-            
-            Button("Button 3") { }
-                .buttonStyle(.borderedProminent)
-            
-            Button("Button 4", role: .destructive) { }
-                .buttonStyle(.borderedProminent)
-            
-            Button("Button 5") { }
-                .buttonStyle(.borderedProminent)
-                .tint(.indigo)
+            VStack {
+                Spacer()
+                
+                Text("Guess The Flag")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundColor(.white)
+                
+                VStack(spacing: 15){
+                    VStack{
+                        Text("Tap the correct flag")
+                            .font(.subheadline.weight(.heavy))
+                        
+                        Text(countries[corretAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    } .foregroundColor(.secondary)
+                    
+                    ForEach(0..<3){ number in
+                        Button{
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .cornerRadius(10)
+                                .shadow(radius: 10)
+                        }.alert(scoreTitle, isPresented: $showingScore) {
+                            Button("Continue", action: askQuestion)
+                        } message: {
+                            Text("Your score is ???")
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: ???")
+                    .foregroundStyle(.white)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/.bold())
+                
+                Spacer()
+            }.padding()
         }
-        
-        // Custom button styling
-        Button {
-            print("Button was pressed")
-        } label: {
-            Text("Tap Me")
-                .frame(width: 80, height: 50)
-                .foregroundColor(.white)
-                .background(.red)
-                .cornerRadius(15)
-        }.padding()
-        
-        Image(systemName: "person")
-        
-        // Button with image and text inside it
-        Button("Edit", systemImage: "person") { }.padding() .buttonStyle(.bordered)
-        
-        // Another representation but with custom building button
-        Button{
-            print("The buton was pressed")
-        } label: {
-            Label("Edit", systemImage: "person")
-                .padding()
-                .foregroundStyle(.white)
-                .background(.red)
-        }
-        
-        Button ("Alert") {
-            alertButton = true
-        } .alert("This is an important Message",isPresented: $alertButton) {
-            // Button("OK") { }  // The alert button is automatically designed to toggle the alert message bool
-            // OR if we want two buttons
-            Button("Delete", role: .destructive) {}
-            Button("Cancel", role: .cancel) {}
-        } message: {
-            Text("Please Read This!!")
-        }
-        
     }
     
-//    func deleteAction() {
-//        print("Delete button pressed")
-//    }
+    func flagTapped(_ number: Int) {
+        if number == corretAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        corretAnswer = Int.random(in: 0...2)
+    }
 }
 
 #Preview {
